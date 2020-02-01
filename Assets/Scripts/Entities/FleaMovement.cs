4392db61769519147ddfc454c584b4037b8f21fa
Oliver <ochamberlai2@gmail.com>
2020-02-01 13:39:46 +0000

    flea spawning + movement

---
+++ Assets/Scripts/Entities/FleaMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FleaMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float wanderRadius;
+    [SerializeField]
+    private float speed = 4f;
+    private Vector2 currentGoal;
+
+    private float step;
+
+    void Start()
+    {
+        step = speed * Time.deltaTime;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Wander();   
+    }
+
+    private void Wander()
+    {
+        //if less than 1 unit away from goal
+        if(Vector2.Distance(transform.position, currentGoal) < 1f)
+        {
+            //get new goal
+            currentGoal = Random.insideUnitCircle * wanderRadius;
+           // if(currentGoal.)
+        }
+        transform.position = Vector2.MoveTowards(transform.position, currentGoal, step);
+    }
+}
---
+++ Assets/Scripts/Managers/SpawningManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawningManager : MonoBehaviour
+{
+    private static SpawningManager instance;
+    public static SpawningManager Instance
+    {
+        get
+        {
+            if (instance == null) {
+                instance = FindObjectOfType<SpawningManager>();
+                if (instance == null)
+                {
+                    GameObject obj = new GameObject();
+                    obj.name = typeof(SpawningManager).Name;
+                    instance = obj.AddComponent<SpawningManager>();
+                }
+            }
+            return instance;
+        }
+    }
+
+    [SerializeField]
+    private Vector2 spawningAreaCentre;
+    
+    public Vector2 spawningArea;//Box where fleas can spawn
+
+    [SerializeField]
+    private Vector2 numFleaSpawnRange = new Vector2(10,20);//The range for number of fleas to be spawned. X is the lower range, Y is the upper range.
+    [SerializeField]
+    private GameObject fleaPrefab;//Prefab for flea to be spawned
+
+    private List<GameObject> fleas;
+
+    public void Start()
+    {
+        SpawnFleas();
+    }
+
+    /*
+        Method to spawn in fleas
+    */
+    public void SpawnFleas()
+    {
+        //Get the final number of fleas to spawn this time round
+        float NumSpawnedFleas = Mathf.Floor(Random.Range(numFleaSpawnRange.x, numFleaSpawnRange.y));
+
+        
+        for(int i = 0; i < NumSpawnedFleas; i++)
+        {
+            //Choose a random spawn area and instantiate the flea
+            Vector2 chosenSpawnArea = new Vector2(Random.Range(-spawningArea.x, spawningArea.x), Random.Range(-spawningArea.y, spawningArea.y));
+            GameObject spawnedFlea = Instantiate(fleaPrefab, chosenSpawnArea,Quaternion.identity);
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireCube(spawningAreaCentre,new Vector3(spawningArea.x*2, spawningArea.y*2,0));
+    }
+
+
+
+
+}
