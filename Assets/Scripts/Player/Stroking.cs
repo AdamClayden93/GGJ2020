4cf1b11e7a9cf28279e799a3d77225826a6b03b2
ClaydenGamedev <e1003984@uos.ac.uk>
2020-02-02 02:11:45 +0000

    Game transitions to all scenes
    TODO:
    scene dark to bright
    trail renderer
    random purr and meows
    
    end state loop back to menu
    
    among other things

--- Assets/Scripts/Player/Stroking.cs
+++ Assets/Scripts/Player/Stroking.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Stroking : MotherSingleton<Stroking>
 {
     AudioClip[] purrArray = new AudioClip[3];
+    bool lerpCamera;
+    Vector3 newPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        newPos = new Vector3(40.69f, 0, 0);
+        lerpCamera = false;
     }
 
     // Update is called once per frame
@@ -19,6 +23,8 @@ public class Stroking : MotherSingleton<Stroking>
         {
             print("HI");
         }
+        if (!lerpCamera)
+        {
             //if (GameManager.Instance.inPhaseTwo)
             //{
             if (Input.touchCount > 0)
@@ -27,10 +33,15 @@ public class Stroking : MotherSingleton<Stroking>
                 if (touch.phase == TouchPhase.Moved)
                 {
                     if (Vector2.Distance(Input.mousePosition, GOReferences.Instance.mainCam.WorldToScreenPoint(Cat.Instance.fleabagLocations[Cat.Instance.fleabagIndex].position)) < 40f)
-                { 
+                    {
                         Handheld.Vibrate();
-                        if(touch.phase == TouchPhase.Ended)
+                        lerpCamera = true;
+                        SceneManager.LoadScene(3);
+                        if (touch.phase == TouchPhase.Ended)
                         {
+                            SceneManager.LoadScene(3);
+                            
+                            //40.69
                             // START FLEA GAME!
                         }
                         // some audio and some animation pls
@@ -38,18 +49,23 @@ public class Stroking : MotherSingleton<Stroking>
                     }
                 }
             }
-            //if (touch.phase == TouchPhase.Moved)
-            //{
-            /*print("mouse:" + GOReferences.Instance.mainCam.ScreenToWorldPoint(Input.mousePosition) + " cat: " + Cat.Instance.fleabagLocations[Cat.Instance.fleabagIndex].position);
-                if (Vector2.Distance(GOReferences.Instance.mainCam.ScreenToWorldPoint(Input.), Cat.Instance.fleabagLocations[Cat.Instance.fleabagIndex].position) < 2f)
-                {
-                print("NICE");
-                    Handheld.Vibrate();
-                    // some audio and some animation pls
-                    // found the fleas!
-                }*/
-            //}
+        }
+        else
+        {
+            //GOReferences.Instance.mainCam.transform.rotation = Quaternion.Lerp(GOReferences.Instance.mainCam.transform.rotation, Quaternion.Euler(newPos), Time.deltaTime * 5);
+        }
+        //if (touch.phase == TouchPhase.Moved)
+        //{
+        /*print("mouse:" + GOReferences.Instance.mainCam.ScreenToWorldPoint(Input.mousePosition) + " cat: " + Cat.Instance.fleabagLocations[Cat.Instance.fleabagIndex].position);
+            if (Vector2.Distance(GOReferences.Instance.mainCam.ScreenToWorldPoint(Input.), Cat.Instance.fleabagLocations[Cat.Instance.fleabagIndex].position) < 2f)
+            {
+            print("NICE");
+                Handheld.Vibrate();
+                // some audio and some animation pls
+                // found the fleas!
+            }*/
         //}
-        
+        //}
+
     }
 }
